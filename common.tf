079fa4e3607fba45e772bc398577c32701c80e60
Vinod Sai Kumar <60657267+vinod-global@users.noreply.github.com>
2021-01-05 13:49:15 +0000

    feat: updated to the latest uuids (#2)

--- common.tf
+++ common.tf
@@ -1,4 +1,3 @@
 module "common" {
-  source = "github.com/global-devops-terraform/global-info?ref=v0.31.0"
+  source = "github.com/global-devops-terraform/global-info?ref=v0.36.0"
 }
-
--- es.tf
+++ es.tf
@@ -4,5 +4,5 @@ module "es" {
   secret_prefix     = "secret/selfservice/envs/${local.environment}/elasticsearch"
   elasticsearch_url = local.elasticsearch_url
   admin_group       = module.common.azure_groups["devops"]
-  user_group        = module.common.azure_groups["devops"]
+  user_group        = module.common.azure_groups["selfservice"]
 }
